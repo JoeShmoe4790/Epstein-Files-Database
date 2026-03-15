623acc98c458a7801fe3334e36def9f1806663cc
JoeShmoe4790 <daniel.shabo44@gmail.com>
2026-03-14 21:43:54 -0700

    Remove config.py and add to gitignore

--- pipeline/config.py
+++
@@ -1,9 +0,0 @@
-ANTHROPIC_API_KEY = "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"
-
-DB_CONNECTION = (
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=SHABOOBY;"
-    "DATABASE=EpsteinFiles;"
-    "Trusted_Connection=yes;"
-    "TrustServerCertificate=yes;"
-)
